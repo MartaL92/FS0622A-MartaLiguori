8f0f3b3b14dc848ee114bf56b309a857bd9dd357
MartaL92 <martaliguori92@gmail.com>
2022-10-10 10:33:04 +0200

    m1/w3/d1

---
+++ m1/w3/d1/assets/js/naming.js
@@ -0,0 +1,26 @@
+//correzione del nome variabile perché iniziava con un numero
+var pet_preferito = 'gatto';
+console.log(pet_preferito);
+
+//correzione nome variabili
+var pet = 'gatto';
+var pet2 = 'cane';
+var petPreferito = 'criceto';
+var petPreferito2 = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//correzione nome e valore variabile e aggiunta di una direttiva di output
+var numero = 12;
+var numero2 = 12.50;
+document.write(numero + numero2);
+
+//nuove variabili e direttive
+var num = 5.6;
+var num2 = 9.32;
+document.write(num * num2);
