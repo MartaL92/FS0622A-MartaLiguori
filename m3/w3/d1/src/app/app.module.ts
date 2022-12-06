6b7661b57e52c4e1a46900e54c5b8dd5f29ce7d8
MartaL92 <martaliguori92@gmail.com>
2022-12-06 09:03:37 +0100

    m3

---
+++ m3/w3/d1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ListCardComponent } from './components/list-card/list-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    ListCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
